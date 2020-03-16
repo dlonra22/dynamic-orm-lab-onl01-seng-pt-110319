2d5305594eb9ba849d7fb4fd8b5e57387ba8e00f
arnie b <arnold.bwaila@gmail.com>
2020-03-16 15:30:24 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -33,9 +33,9 @@ class InteractiveRecord
  #   cols = self.class.column_names.delete_if {|col| col == "id"}
  #    cols.join(",")
  #end
- def col_names_for_insert
-    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
- end
+ #def col_names_for_insert
+ #   self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+ #end
  
   def values_for_insert
       values = []
